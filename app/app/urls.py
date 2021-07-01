6344f0c9cec868ea1c0c0d7ee0e9a65c90212ad2
dinner988 <noriki.ikeuchi@ucl.meijo-u.ac.jp>
2021-07-01 15:10:32 +0900

    追加
    #5の内容追加

--- app/app/urls.py
+++ app/app/urls.py
@@ -4,7 +4,6 @@ from django.urls import include, path
 # 全体のルーティングをィングを示す
 urlpatterns = [
     # サーバアドレスの後に続くアドレスによってルーディングを変化
-    path('hello/', include('bbs.urls')),
     path('bbs/', include('bbs.urls')),
     path('admin/', admin.site.urls),
 ]
--- app/bbs/views.py
+++ app/bbs/views.py
@@ -3,5 +3,10 @@ from django.http import HttpResponse
 
 def index(request):
     # return HttpResponse('Hello world')
+    context = {
+        'message':'Welcome my BBS',
+        'players':['勇者','戦士','魔法使い','忍者']
+    }
+
     # render:データとテンプレートを組み合わせてwebページを返すショートカット関数
-    return render(request, 'bbs/index.html')
+    return render(request, 'bbs/index.html',context)
